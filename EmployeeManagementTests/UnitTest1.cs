c1a0ad20005f6e2b0e9c0d42720fce8d0a340ea0
mate <abulashvilimate@gmail.com>
2023-06-09 04:28:26 -0700

    added tests

---
+++ EmployeeManagementTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EmployeeManagementTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
